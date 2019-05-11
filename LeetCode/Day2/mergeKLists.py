dce6c74016515c94f6c9fb524793558d46b092cc
Bruce Wang <486590722@qq.com>
2019-05-11 09:06:37 +0800

    Update mergeKLists.py
    The former version just doesn't work out because of unclean definition of data structure and functions. However, after thinking about the 
     python equality of pointer struct in C-like language, I got this design. Or, correct the foolish codes.

--- LeetCode/Day2/mergeKLists.py
+++ LeetCode/Day2/mergeKLists.py
@@ -1,105 +1,76 @@
-class LinkedList:
+class Node:
 	def __init__(self, data):
 		self.data = data
 		self.next = None
 
-def printList(head):
-	while head != None:
-		print(head.data)
-		head = head.next
+class LinkedList:
+	def __init__(self):
+		self.head = None
 
-def getLength(head):
-	cnt = 0
-	while head != None:
-		cnt += 1
-		head = head.next
-	return cnt
+	def printList(self):
+		temp = self.head
+		while temp:
+			print(temp.data, end='->')
+			temp = temp.next
 
-# RecursionError: maximum recursion depth exceeded
-# a and b are two linked lists
-def sortMergeRecur(a, b):
-	result = LinkedList(None)
-	if a == None:
-		return b
-	if b == None:
-		return a
+	def append(self, data):
+		new_node = Node(data)
 
-	# pick either a or b, and recur
-	if a.data <= b.data:
-		result = a
-		result.next = sortMerge(a, b)
+		if self.head == None:
+			self.head = new_node
+			return
+		last = self.head
+		while last.next:
+			last = last.next
+		last.next = new_node
+
+# a and b are LinkedList class instance
+def sortMerge(head1, head2):
+	temp = None
+	if head1 == None:
+		return head2
+	if head2 == None:
+		return head1
+	if head1.data <= head2.data:
+		temp = head1
+		temp.next = sortMerge(head1.next, head2)
 	else:
-		result = b
-		result.next = sortMerge(a, b)
+		temp = head2
+		temp.next = sortMerge(head1, head2.next)
+	return temp
 
-	return result
-
-def sortMerge(a, b):
-	head = LinkedList(None)
-	temp = head
-	if a == None:
-		return b
-	if b == None:
-		return a
-	while a!=None and b!=None:
-		if a.data <= b.data:
-			# temp.data = a.data
-			temp = a
-			print(temp.data)
-			temp = temp.next
-			a = a.next
-		else:
-			# temp.data = b.data
-			temp = b
-			print(temp.data)
-			temp = temp.next
-			b = b.next
-
-	if a != None:
-		temp = a
-	if b != None:
-		temp = b
-	return head
-
-
-
-# arr stores array of linked lists
-# last means how many list(s) are/is left
 def mergeKLists(arr, last):
-	while last != 0:
+	while last!=0:
 		i = 0
 		j = last
-		# K/2 every round until there is only one list left
 		while i<j:
-			# arr[i] = sortMergeRecur(arr[i], arr[j])
-			arr[i] = sortMerge(arr[i], arr[j])
+			arr[i].head = sortMerge(arr[i].head, arr[j].head)
 			i += 1
 			j -= 1
-			if i >= j:
+			if i>=j:
 				last = j
-	return arr[0]
+	return arr[0].head
 
 if __name__ == '__main__':
 	k = 3
-	arr = [None for i in range(k)]
+	arr = [LinkedList() for i in range(k)]
 
-	arr[0] = LinkedList(1)
-	arr[0].next = LinkedList(3)
-	arr[0].next.next = LinkedList(5)
-	arr[0].next.next.next = LinkedList(7)
+	arr[0].append(1)
+	arr[0].append(3)
+	arr[0].append(5)
+	arr[0].append(7)
 
-	arr[1] = LinkedList(2)
-	arr[1].next = LinkedList(4)
-	arr[1].next.next = LinkedList(6)
-	arr[1].next.next.next = LinkedList(8)
+	arr[1].append(2)
+	arr[1].append(4)
+	arr[1].append(6)
+	arr[1].append(8)
 
-	arr[2] = LinkedList(0)
-	arr[2].next = LinkedList(9)
-	arr[2].next.next = LinkedList(10)
-	arr[2].next.next.next = LinkedList(11)
+	arr[2].append(0)
+	arr[2].append(9)
+	arr[2].append(10)
+	arr[2].append(11)
 
-	# printList(arr[0])
+	llist = LinkedList()
+	llist.head = mergeKLists(arr, k-1)
 
-	head = mergeKLists(arr, k-1)
-
-	printList(head)
+	llist.printList()
